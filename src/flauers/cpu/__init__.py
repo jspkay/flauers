08d0020e9f7821cc66e807e8cd09a3a3b32f2a8e
spappalardo <jspkay@gmail.com>
2024-06-07 15:20:05 +0200

    renamed

---
+++ src/flauers/cpu/__init__.py
@@ -0,0 +1,6 @@
+from .matmuls_float import \
+    injected_matmul_old_f32, injected_matmul_old_f64, \
+        injected_matmul_old_f32_parallel, injected_matmul_old_f64_parallel
+from .mamtuls_int import \
+    injected_matmul_old_int, injected_matmul_old_int_parallel
+from .injects import *
--- src/flauers/cpu/injects.py
+++ src/flauers/cpu/injects.py
@@ -4,7 +4,7 @@ from numba import njit, int64, int32, int16, int8
 # General version for integers
 # Note that the dtype is different for the accumulation and for the inputs
 # so jit should generate a different version for each (in_dtype, mac_dtype) pair
-@njit(nogil=True, cache=True)
+@njit(nogil=True, cache=False)
 def inject_int(value, bitstring, type):
     if type == 0:  # stuck-at 0
         return value & ~bitstring
@@ -17,7 +17,7 @@ def inject_int(value, bitstring, type):
     return bitstring
 
 # Explicit versions for floats
-@njit( int32(int32, int32, int8), nogil=True, cache=True)
+@njit( int32(int32, int32, int8), nogil=True, cache=False)
 def inject_32(value: np.int32, bitstring: np.int32, type: np.int8):
     if type == 0:  # stuck-at 0
         return value & ~bitstring
@@ -29,14 +29,20 @@ def inject_32(value: np.int32, bitstring: np.int32, type: np.int8):
     # bizantine neuron
     return bitstring
 
-@njit( int64(int64, int64, int8), nogil=True, cache=True)
+@njit( int64(int64, int64, int8), nogil=True, cache=False)
 def inject_64(value: np.int64, bitstring: np.int64, type: np.int8):
+    if bitstring != 0:
+        print("injecting value ", value)
+
     if type == 0:  # stuck-at 0
         return value & ~bitstring
     if type == 1:  # stuck-at 1
         return value | bitstring
     if type == 2:  # bit-flip
         return value ^ bitstring
+
+    if bitstring != 0:
+        print("new value is ", value)
 
     # bizantine neuron
     return bitstring
