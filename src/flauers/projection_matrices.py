13f2434ddb371edef8a5463819987be246d91478
spappalardo <jspkay@gmail.com>
2024-06-17 14:42:56 +0200

    Fixed minor bugs

--- src/flauers/projection_matrices.py
+++ src/flauers/projection_matrices.py
@@ -23,8 +23,8 @@ row_stationary = np.array(
 )
 row_stationary_eq = np.array(
     [
-        [1, 0,  0],
-        [0,  0, -1],
+        [-1, 0,  0],
+        [0,  0, 1],
         [1,  1, 1]
     ]
 )
